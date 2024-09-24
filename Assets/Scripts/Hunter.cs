91d9ccce6819f17d9ec44adf8a27b840a99fa73c
Anngelous <angelobarchada@gmail.com>
2024-09-24 05:01:40 -0300

    patrol hunter

--- Assets/Scripts/Hunter.cs
+++ Assets/Scripts/Hunter.cs
@@ -8,10 +8,26 @@ public class Hunter : MonoBehaviour
     public bool Energia;
     public bool DistanciaRebaño;
 
+    Vector3 _velocity;
+    [SerializeField] float _maxVelocity;
+    [Range(0f, 1f)]
+    [SerializeField] float _maxSteering;
+    [SerializeField] int currentTarget;
+    [SerializeField] Transform[] targets;
+    [SerializeField] float distanceThreshold;
+
 
     private void Update()
     {
         rootNode.ExecuteHunter(this);
+
+        transform.position += _velocity * Time.deltaTime;
+        transform.forward = _velocity;
+
+        if(Vector3.Distance(transform.position, targets[currentTarget].position) < distanceThreshold)
+        {
+            NextTarget();
+        }
     }
     public void Descansar()
     {
@@ -21,10 +37,35 @@ public class Hunter : MonoBehaviour
     public void Patrullar()
     {
         Debug.Log("Esta patrullando");
+        Seek(targets[currentTarget].position);
     }
 
     public void Perseguir()
     {
         Debug.Log("Esta persiguiendo");
+
+    }
+
+    public void AddForce(Vector3 dir)
+    {
+        _velocity = Vector3.ClampMagnitude(_velocity + dir, _maxVelocity);
+    }
+
+    public void Seek(Vector3 target)
+    {
+        Debug.Log("Estoy yendo a la fruta");
+        Vector3 desiredVelocity = target - transform.position;
+        desiredVelocity.Normalize();
+        desiredVelocity *= _maxVelocity;
+
+        Vector3 steeringVelocity = desiredVelocity - _velocity;        
+        steeringVelocity = Vector3.ClampMagnitude(steeringVelocity, _maxSteering);
+        AddForce(steeringVelocity);
+    }
+
+    public void NextTarget()
+    {
+        Debug.Log("Cambio de target");
+        currentTarget = (currentTarget + 1) % targets.Length;
     }
 }
